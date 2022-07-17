e77e96f1ee36066254ba44b5732be1f0ed831297
livenott <jelizavetaou@gmail.com>
2022-07-17 04:43:33 +0300

    added: map, review form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
-const title = document.createElement('h3');
+// Импорт стилей
 import '../src/style.css';
 
-title.textContent = 'Webpack';
-const page = document.querySelector('body');
-page.append(title);
+let myMap;
+let reviewForm = document.querySelector('.map__review');
 
+const init = () => {
+    myMap = new ymaps.Map("map", {
+        center: [35.337272, 33.322556],
+        zoom: 15,
+        controls: []
+    });
 
+    const coords = [
+        [35.338573, 33.315818],
+        [35.336780, 33.307021]
+    ];
+
+    var myCollection = new ymaps.GeoObjectCollection({}, {
+        preset: 'islands#redIcon', //все метки красные
+        draggable: false // и их можно перемещать
+    });
+
+    coords.forEach(coord => {
+        myCollection.add(new ymaps.Placemark(coord));
+    });
+
+    myMap.geoObjects.add(myCollection);
+
+    myMap.behaviors
+    .disable(['drag', 'rightMouseButtonMagnifier','scrollZoom','dblClickZoom'])
+
+    myMap.events.add('click', function (e) {
+        if(reviewForm.classList.contains('d-none')){
+            reviewForm.classList.remove('d-none');
+        }else{
+            reviewForm.classList.add('d-none');
+        }
+    })
+}
+
+ymaps.ready(init);
